958ca8db94a78ceb63dac2d802a251c1311c0750
Baptiste VERDIER <baptiste.verdier@manomano.com>
2023-07-17 17:07:37 +0200

    feat(homepage): first commit to set up the project in a clean way, à la mano

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import DashboardExample from "/src/components/Dashboard/Dashboard";
+import Header from "/src/components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <DashboardExample />
+    </>
+  );
+}
+
+export default App;
